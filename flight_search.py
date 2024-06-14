1553b46c486be5b5900c7ef2f099dc7f1d555da7
Nemanja Markovic <necamark@gmail.com>
2024-06-15 01:05:31 +0200

    "getting IATA codes using Amadeus API; populate codes into Google Sheets"

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,47 @@
+import requests
+
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+CITY_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        self._api_key = "uOStceKl5K3VGYJww4dBBkFowjxWpNMc"
+        self._api_secret = "Yw09Uzkx1zBWp2Xi"
+        self._token = self._get_new_token()
+
     def get_destination_code(self, city_name):
-        code = "TESTING"
+        headers = {
+            "Authorization": f"Bearer {self._token}"
+        }
+        parameters = {
+            "keyword": city_name.upper(),
+            "max": 2,
+            "include": "AIRPORTS"
+        }
+        response = requests.get(url=CITY_ENDPOINT, params=parameters, headers=headers)
+        try:
+            code = response.json()["data"][0]['iataCode']
+        except IndexError:
+            print(f"IndexError: No airport code found for {city_name}.")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for {city_name}.")
+            return "Not Found"
         return code
+
+    def _get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': self._api_key,
+            'client_secret': self._api_secret
+        }
+
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+        response.raise_for_status()
+        return response.json()['access_token']
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes
 # to achieve the program requirements.
-
+from flight_search import FlightSearch
 from data_manager import DataManager
+import time
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 # print(sheet_data)
+flight_search = FlightSearch()
 
-if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
-    for row in sheet_data:
+
+for row in sheet_data:
+    if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    # print(f"sheet data:\n {sheet_data}")
+    time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
 
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
 
