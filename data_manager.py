a98e8b0fdc2d66312e6403be60510c0b30d9b8bf
Nemanja Markovic <necamark@gmail.com>
2024-06-15 00:18:07 +0200

    "using Sheety API to read and write data into Google Sheet"

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,33 @@
 import requests
 
+BEARER_TOKEN = "kdfjsklapo1kjis02js%jsi12"
 SHEET_ENDPOINT = "https://api.sheety.co/643dccdcddb0204a103d7f46ce9f9a94/flightDeals/prices"
+
+headers = {
+    "Authorization": f"Bearer {BEARER_TOKEN}"
+}
 
 
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
-    ...
+    def __init__(self):
+        self.destination_data = {}
 
+    def get_destination_data(self):
+        response = requests.get(url=SHEET_ENDPOINT, headers=headers)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
 
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEET_ENDPOINT}/{city["id"]}",
+                json=new_data,
+                headers=headers
+            )
