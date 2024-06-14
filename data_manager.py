d0e2fd99b76386cf3e318c6ea329a4c5d4b1032d
Nemanja Markovic <necamark@gmail.com>
2024-06-14 23:30:26 +0200

    "staring project"

---
+++ data_manager.py
@@ -0,0 +1,10 @@
+import requests
+
+SHEET_ENDPOINT = "https://api.sheety.co/643dccdcddb0204a103d7f46ce9f9a94/flightDeals/prices"
+
+
+class DataManager:
+    # This class is responsible for talking to the Google Sheet.
+    ...
+
+
