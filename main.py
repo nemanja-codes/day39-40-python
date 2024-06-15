5c888c907334c2ec5a1f6efa3182b8263cdddb89
Nemanja Markovic <necamark@gmail.com>
2024-06-15 23:44:27 +0200

    "structure main.py little better"

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from notification_manager import NotificationManager
 import time
 from datetime import datetime, timedelta
 
-ORIGIN_CITY_IATA = "LON"
+
+# ==================== Set up the Flight Search ====================
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 # print(sheet_data)
 flight_search = FlightSearch()
 
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
+
+# ==================== Update the Airport Codes in Google Sheet ====================
 
 for row in sheet_data:
     if row["iataCode"] == "":
@@ -24,6 +29,8 @@ for row in sheet_data:
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
+
+# ==================== Search for Flights and Send Notifications ====================
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
