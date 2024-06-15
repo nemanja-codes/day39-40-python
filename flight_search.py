ca74f8d1b8a101af8f8778954e5750661ec44914
Nemanja Markovic <necamark@gmail.com>
2024-06-15 23:13:29 +0200

    "search for cheapest flights"

--- flight_search.py
+++ flight_search.py
@@ -2,6 +2,7 @@ import requests
 
 TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 CITY_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 
 
 class FlightSearch:
@@ -45,3 +46,28 @@ class FlightSearch:
         response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
         response.raise_for_status()
         return response.json()['access_token']
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        header = {
+            'Authorization': f'Bearer {self._token}'
+        }
+
+        parameters = {
+            'originLocationCode': origin_city_code,
+            'destinationLocationCode': destination_city_code,
+            'adults': "1",
+            'nonStop': "true",
+            'departureDate': from_time.strftime("%Y-%m-%d"),
+            'returnDate': to_time.strftime("%Y-%m-%d"),
+            'currencyCode': 'GBP',
+            'max': "10",
+        }
+
+        response = requests.get(url=FLIGHT_ENDPOINT, params=parameters, headers=header)
+
+        if response.status_code != 200:
+            print(f"check_flight() response code: {response.status_code}")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 # to achieve the program requirements.
 from flight_search import FlightSearch
 from data_manager import DataManager
+from flight_data import find_cheapest_flight
 import time
+from datetime import datetime, timedelta
+
+ORIGIN_CITY_IATA = "LON"
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
@@ -13,9 +17,27 @@ flight_search = FlightSearch()
 for row in sheet_data:
     if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
+    # slowing down requests to avoid rate limit
     time.sleep(2)
-print(f"sheet_data:\n {sheet_data}")
+# print(f"sheet_data:\n {sheet_data}")
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination["city"]}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        tomorrow,
+        six_month_from_today
+    )
+    # print(flights)
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
 
