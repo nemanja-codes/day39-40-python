9a2336906995725c52beedc97587962b78348ae3
Nemanja Markovic <necamark@gmail.com>
2024-06-19 01:08:26 +0200

    "notify customers when there is a good deal"

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flight_data import find_cheapest_flight
 from notification_manager import NotificationManager
 import time
 from datetime import datetime, timedelta
+import smtplib
+
 
 
 # ==================== Set up the Flight Search ====================
@@ -30,6 +32,8 @@ for row in sheet_data:
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
 
+customer_emails = data_manager.get_customer_email()
+
 # ==================== Search for Flights and Send Notifications ====================
 
 tomorrow = datetime.now() + timedelta(days=1)
@@ -49,12 +53,6 @@ for destination in sheet_data:
     # Slowing down requests to avoid rate limit
     time.sleep(2)
     notification_manager = NotificationManager()
-    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
-        print(f"Lower price found to {destination["city"]}")
-        notification_manager.send_sms(f"Low price alert! Only {cheapest_flight.price}"
-                                      f" to fly from {cheapest_flight.origin_airport}"
-                                      f" to  {cheapest_flight.destination_airport}, on {cheapest_flight.out_date}"
-                                      f" until {cheapest_flight.return_date}.")
 
     if cheapest_flight.price == "N/A":
         print(f"No direct flight to {destination['city']}. Looking for indirect flights...")
@@ -68,5 +66,20 @@ for destination in sheet_data:
         cheapest_flight = find_cheapest_flight(stopover_flights)
         print(f"Cheapest indirect flight price is: £{cheapest_flight.price}")
 
-customer_emails = data_manager.get_customer_email()
-# print(customer_emails)
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        # Customise the message depending on the number of stops
+        if cheapest_flight.stops == 0:
+            message = f"Low price alert! Only GBP {cheapest_flight.price} to fly direct " \
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, " \
+                      f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        else:
+            message = f"Low price alert! Only GBP {cheapest_flight.price} to fly " \
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, " \
+                      f"with {cheapest_flight.stops} stop(s) " \
+                      f"departing on {cheapest_flight.out_date} and returning on {cheapest_flight.return_date}."
+
+        print(f"Check your email. Lower price flight found to {destination['city']}!")
+
+        notification_manager.send_sms(message)
+        notification_manager.send_emails(customer_emails, message)
+
--- notification_manager.py
+++ notification_manager.py
@@ -1,13 +1,18 @@
 from twilio.rest import Client
+import smtplib
 
 TWILIO_SID = "AC460ce69a64b5a2ba8a8572e00ab9dc44"
-TWILIO_AUTH_TOKEN = "8a080d24160dc8699aee5fc025eac0bb"
+TWILIO_AUTH_TOKEN = "bc9f14286a593b3ec5ffa2223fd626b4"
+
+MY_EMAIL = "necamark@gmail.com"
+PASSWORD = "xuklpbmootnalxdi"
 
 
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
         self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+        self.connection = smtplib.SMTP(host="smtp.gmail.com", port=587)
 
     def send_sms(self, message_body):
         message = self.client.messages \
@@ -18,3 +23,13 @@ class NotificationManager:
         )
 
         print(message.sid)
+
+    def send_emails(self, email_list, email_body):
+        self.connection.starttls()
+        self.connection.login(user=MY_EMAIL, password=PASSWORD)
+        for email in email_list:
+            self.connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=email,
+                msg=f"Subject:New Low Price Flight!\n\n{email_body}".encode('utf-8')
+            )
